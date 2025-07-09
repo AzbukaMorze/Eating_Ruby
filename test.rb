468ef284059f8ac686c5a0e7ac6b71def5f340ff
AzbukaMorze <emelyanov.sm2002@gmail.com>
2025-07-09 14:29:21 +0300

    add factoriallize app

---
+++ test.rb
@@ -0,0 +1,14 @@
+greetings_message = "Welcome to factorial calculation Ruby application!"
+
+puts greetings_message
+
+def factoriallize(n)
+    return 1 if n == 0
+    n * factoriallize(n - 1)
+end
+
+puts "Type a number to factoriallize: "
+
+number = gets.to_i
+
+puts factoriallize(number)
